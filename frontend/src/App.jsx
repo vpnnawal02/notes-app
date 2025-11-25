ef7d50c8a2a88ee41fee3bb4bb58617df2a5821c
Vipin <vn.nawal02@gmail.com>
2025-11-26 01:58:17 +0530

    made changes to app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,56 +1,53 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate, Form } from "react-router-dom"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Home from './pages/Home'
-import NotFound from './pages/NotFound'
-// import Form from './components/Form'
-import ProtectedRoute from './components/ProtectedRoute'
-import Navbar from './components/NavigationBar'
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Form from "./components/Form";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+// Logout clears tokens and redirects
 function Logout() {
-  localStorage.clear()
-  return <Navigate to="/login" />
-}
-
-function RegisterAndLogout() {
-  localStorage.clear()
-  return <Register />
-
+  localStorage.clear();
+  return <Navigate to="/login" />;
 }
 
 function App() {
-
   return (
-    <>
-      <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
+    <BrowserRouter>
+      <Routes>
 
-          {/* <Route path='/login' element={<Login />} /> */}
-          <Route path="/login" element={<Form route="/api/token/" method="login" />} />
+        {/* HOME (protected) */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
 
-          <Route path='/logout' element={<Logout />} />
+        {/* LOGIN */}
+        <Route
+          path="/login"
+          element={<Form route="/api/token/" method="login" />}
+        />
 
-          {/* <Route path='/register' element={<RegisterAndLogout />} /> */}
-          <Route path="/register" element={<Form route="/api/user/register/" method="register" />} />
+        {/* REGISTER */}
+        <Route
+          path="/register"
+          element={<Form route="/api/user/register/" method="register" />}
+        />
 
-          <Route path='*' element={<NotFound />} />
+        {/* LOGOUT */}
+        <Route path="/logout" element={<Logout />} />
 
-          {/* testing */}
-          {/* <Route path='/form' element={<Form />} /> */}
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+        {/* 404 PAGE */}
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
